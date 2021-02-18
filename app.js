6a3ea1a459fce6ab6c5bdc476f59fdb1d4bea935
Mayanklna <mp410081@gmail.com>
2021-02-18 15:35:33 +0530

    try

---
+++ app.js
@@ -0,0 +1,6 @@
+const map=L.map('map').setView([22.9074872, 79.07306671],5);
+const tileUrl='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const attribution='&copy; <a href="https://www.iitr.ac.in/">IIT Roorkee</a>❤️';
+const tileLayer = L.tileLayer(tileUrl, {  attribution });
+tileLayer.addTo(map);
+
