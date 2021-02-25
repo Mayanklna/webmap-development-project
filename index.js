6050546747f91cb8783747a72d7bf6016752adc8
Mayanklna <mp410081@gmail.com>
2021-02-25 22:03:39 +0530

    some change

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express=require('express');
+const cors = require('cors');
 const app=express();
 const PORT=process.env.PORT ||3000
 var path = require('path');
  
-
+app.use(cors());
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
  
+ 
 app.listen(PORT, () => {
+  
   console.log(`Example app listening at http://localhost:${PORT}`)
 })
